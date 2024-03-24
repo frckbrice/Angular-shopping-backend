cc45d95fa83b4934c70e93a360f6bd941bc5f413
frckbrice <maebrie2017@gmail.com>
2024-03-24 11:13:58 +0100

    update public link

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = 3000;
 
 // Cors configuration - Allows requests from localhost:4200
 const corsOptions = {
-  origin: ["http://localhost:4200", "https://vercel.com/frckbrice/angular-shopping-app"],
+  origin: ["http://localhost:4200", "https://angular-shopping-app.vercel.app/"],
   optionsSuccessStatus: 204,
   methods: "GET, POST, PUT, DELETE",
 };
